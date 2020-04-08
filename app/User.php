659e3f31d65197b9d77d29519ffaaeadf8b5726e
Alex <alexmil2004@mail.ru>
2020-04-08 19:53:50 +0300

    08-04

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'last_name', 'photo', 'role',
+        'name', 'email', 'password', 'last_name', 'photo', 'role', 'is_admin',
     ];
 
     /**
@@ -36,4 +36,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function idAdmin()
+    {
+        return $this->isAdmin;
+    }
 }
