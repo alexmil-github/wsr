bae36ee63fb434e345be52122e9e57b212b68bfd
Alex <alexmil2004@mail.ru>
2020-04-27 23:16:38 +0300

    27-04

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,6 +27,7 @@
         <nav class="navbar navbar-expand navbar-dark flex-column flex-md-row bg-primary shadow-lg">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
+                    <img src="/images/logo.png" height="70" alt="">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
